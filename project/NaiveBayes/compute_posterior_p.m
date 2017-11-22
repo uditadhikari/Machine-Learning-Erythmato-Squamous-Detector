432bc4ad997cf3d38d8574c08ff380523f25f17c
uditadhikari <Skywalker117>
2017-11-22 14:00:47 -0500

    copmute posterior

--- project/NaiveBayes/compute_posterior_p.m
+++ project/NaiveBayes/compute_posterior_p.m
@@ -1,4 +1,4 @@
-function [posterior_p_psoriasis,  posterior_p_seboreic,posterior_p_lichen,posterior_p_pityriasisRosea,posterior_p_cronicdermatitis,posterior_p_pityriasisRubraPilaris] = compute_posterior_p(data, expectation_Psoriasis,variance_Psoriasis, expectation_Seboreic,variance_Seboreic, expectation_Lichen, variance_Lichen,expectation_ptyriasisRosea,variance_ptyriasisRosea,expectation_cronisdermatitis,variance_expectation_cronisdermatitis,expectation_pityriasisRubraPilaris,variance_pityriasisRubraPilaris)
+function [posterior_p_psoriasis,  posterior_p_seboreic,posterior_p_lichen,posterior_p_pityriasisRosea,posterior_p_cronicdermatitis,posterior_p_pityriasisRubraPilaris] = compute_posterior_p(data, expectation_Psoriasis,variance_Psoriasis, expectation_Seboreic,variance_Seboreic, expectation_Lichen, variance_Lichen,expectation_ptyriasisRosea,variance_ptyriasisRosea,expectation_cronisdermatitis,variance_cronisdermatitis,expectation_pityriasisRubraPilaris,variance_pityriasisRubraPilaris)
 posterior_p_psoriasis = zeros(size(X));
 posterior_p_seboreic = zeros(size(X));
 posterior_p_lichen= zeros(size(X));
@@ -18,9 +18,13 @@ for i = 1:size(X,1),
 end;
 
 for i = 1:size(X,1),
-	posterior_p_pityriasisRosea(i, :) = 1 ./ ((2 * pi) .* variance_Lichen).^ 0.5 .* exp(- ((X(i, :) - expectation_Lichen) .^ 2) ./ (2 .* variance_Lichen));
+	posterior_p_pityriasisRosea(i, :) = 1 ./ ((2 * pi) .* variance_ptyriasisRosea).^ 0.5 .* exp(- ((X(i, :) - expectation_ptyriasisRosea) .^ 2) ./ (2 .* variance_ptyriasisRosea));
 end;
 
 for i = 1:size(X,1),
-	posterior_p_cronicdermatitis(i, :) = 1 ./ ((2 * pi) .* variance_Lichen).^ 0.5 .* exp(- ((X(i, :) - expectation_Lichen) .^ 2) ./ (2 .* variance_Lichen));
+	posterior_p_cronicdermatitis(i, :) = 1 ./ ((2 * pi) .* variance_cronisdermatitis).^ 0.5 .* exp(- ((X(i, :) - expectation_cronisdermatitis) .^ 2) ./ (2 .* variance_cronisdermatitis));
+end;
+
+for i = 1:size(X,1),
+	posterior_p_pityriasisRubraPilaris(i, :) = 1 ./ ((2 * pi) .* variance_pityriasisRubraPilaris).^ 0.5 .* exp(- ((X(i, :) - expectation_pityriasisRubraPilaris) .^ 2) ./ (2 .* variance_pityriasisRubraPilaris));
 end;
