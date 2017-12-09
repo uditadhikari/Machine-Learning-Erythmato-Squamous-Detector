005b39e6f8f6226f50f7c2e1d9c5ecf14e3ad0b7
uditadhikari <Skywalker117>
2017-12-09 10:45:07 -0500

    Commit December 9th, 2017 10:45 AM

--- project/NaiveBayes/run.m
+++ project/NaiveBayes/run.m
@@ -10,36 +10,28 @@ normalized_data = data(: , 1 : 34);
 min_data    = min(normalized_data); 
 max_data    = max(normalized_data);
 range_data  = max_data - min_data;
+mean_data   = mean(normalized_data);
  
-normalized_data = (normalized_data .- min_data) ./ range_data;
+normalized_data = (normalized_data .- mean_data) ./ range_data;
 
-
-X = normalized_data(1:280 , 1 : 34);
+X = normalized_data(1:280 , : );
 y = data(1:280 , 35);
 
-test_data_without_output = normalized_data(281:358,1:34);
+test_data_without_output = normalized_data(281:358, : );
 test_data_output_only = data(281:358,35);
 
 %save prediction.mat prediction;
-
-
-%calculating the accuracy of the training data
-
-
-
-
-prediction = naive_bayes_classifier(X, y, test_data_without_output);
-
-disp("###############Accuracy of Test Data#################")
-
-accuracy(prediction,(358-281),test_data_output_only)
 
 
 prediction = naive_bayes_classifier(X, y, X);
 
 disp("###############Accuracy of Training Data#################")
 
-accuracy(prediction,(280-1),y)
+accuracy(prediction, 280 ,y)
 
+prediction = naive_bayes_classifier(X, y, test_data_without_output);
 
+disp("###############Accuracy of Test Data#################")
+
+accuracy(prediction,(358-280),test_data_output_only)
 
